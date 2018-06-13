29e300bbeff717abf71d3d7bec322fc6432ae333
abekkhozha <abekkhozha@greet-go.com>
2018-06-13 12:16:57 +0600

    cleaned from police check service

--- sandbox.db/src/kz/greetgo/sandbox/db/in_service/model/LegalPersonCheckParams.java
+++
@@ -1,6 +0,0 @@
-package kz.greetgo.sandbox.db.in_service.model;
-
-public class LegalPersonCheckParams {
-  public String name;
-  public String kpp;
-}
--- sandbox.db/src/kz/greetgo/sandbox/db/in_service/model/NaturalPersonCheckParams.java
+++
@@ -1,9 +0,0 @@
-package kz.greetgo.sandbox.db.in_service.model;
-
-public class NaturalPersonCheckParams {
-  public String surname, name, patronymic;
-
-  public String iin;
-
-  public boolean needFinanceCheck;
-}
